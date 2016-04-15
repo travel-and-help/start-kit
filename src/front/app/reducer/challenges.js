9992b6f658c279aa0e07f9387ace60bc1c08fc15
Maksym Lysenko <Maksym_Lysenko@epam.com>
2016-04-15 14:38:24 +0300

    reducers tests

--- src/front/app/reducer/challenges.js
+++ src/front/app/reducer/challenges.js
@@ -1,7 +1,7 @@
 const reducer = (state = [], action = {}) => {
     switch (action.type) {
     case 'GET_INITIAL_CHALLENGES':
-        return action.challenges;
+        return [ ...action.challenges ];
     default:
         return state;
     }
