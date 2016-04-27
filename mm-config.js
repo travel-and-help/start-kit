42a78da65c5e48457b5aee21fe8a1ca948d67a05
Borys Ihnatyev <paur.link@gmail.com>
2016-04-27 21:27:42 +0300

    #68 feature(env) add database username and password environment variables

--- mm-config.js
+++ mm-config.js
@@ -1,9 +1,13 @@
+'use strict';
+
 const env = require('./env');
 
 module.exports = {
     db: env.DB_NAME,
     host: env.DB_HOST,
     port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
     collection: 'migrations',
     directory: './migrations'
 };
