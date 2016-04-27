f7533011ddf9481c008cf58d1347a87d779da761
Sergii Gorbenko <Sergii_Gorbenko@epam.com>
2016-04-27 18:15:30 +0300

    #17 Selection of challenge topic upon login (FE)

--- src/back/controllers/api/routes/challengeTopics/index.spec.js
+++ src/back/controllers/api/routes/challengeTopics/index.spec.js
@@ -6,7 +6,7 @@ describe('challenge topics route', () => {
     it('should get all topics for root', () => {
         const challengeTopics = { getAll: 'smth' };
         const get = env.stub();
-        const Router = env.stub().returns({ get });
+        const Router = env.stub().returns({ get: get });// eslint-disable-line
         proxyquire('./index', {
             express: { Router },
             './challengeTopics': challengeTopics
