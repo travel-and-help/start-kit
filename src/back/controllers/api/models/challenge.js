fdb048af5ec4a98fbcff9af05c6d87cc29cc0e60
oleksii-novakov <novakof@gmail.com>
2016-04-27 12:46:22 +0300

    Migration for challenge topics

--- src/back/controllers/api/models/challenge.js
+++ src/back/controllers/api/models/challenge.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Challenge = new Schema({
-    category: { type: String, required: true },
-    description: { type: String, required: true },
+    category: { type: Array, required: true },
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    level: { type: String, required: true },
     location: { type: String, required: true },
-    user: { type: Object, required: true }
+    user: { type: Object, required: true },
+    description: { type: String, required: true }
 });
 
-module.exports = mongoose.model('Challenge', Challenge);
+module.exports = mongoose.model('Challenge', Challenge, 'challenges');
