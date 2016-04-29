027ffda86e00c2a4aea463e713875d4424a050e3
oleksii-novakov <novakof@gmail.com>
2016-04-29 23:16:29 +0300

    Added common folder for global components and styles (preprocessor mixins and functions)

---
+++ src/front/app/components/common/IconButton/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const IconButton = ({
+    title,
+    iconName,
+    clickHandler,
+    iconSize = 24,
+    iconClassName = '',
+    buttonClassName = '',
+    buttonType = 'button'
+}) => (
+    <button type={buttonType} onClick={clickHandler} className={'icon-button ' + buttonClassName}>
+        <i className={'icon icon_size-' + iconSize + ' ' + iconClassName}>{iconName}</i>
+        {title}
+    </button>
+);
+
+IconButton.propTypes = {
+    title: React.PropTypes.string,
+    iconName: React.PropTypes.string.isRequired,
+    clickHandler: React.PropTypes.func,
+    iconSize: React.PropTypes.number,
+    iconClassName: React.PropTypes.string,
+    buttonClassName: React.PropTypes.string,
+    buttonType: React.PropTypes.string
+};
+
+export default IconButton;
