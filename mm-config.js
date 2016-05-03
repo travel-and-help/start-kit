da31c25a98ad3e59bac2dc96792b51fe678b9fec
Borys Ihnatyev <paur.link@gmail.com>
2016-05-03 21:26:24 +0300

    fix(mm-config) enocode user name and password

--- mm-config.js
+++ mm-config.js
@@ -6,8 +6,8 @@ module.exports = {
     db: env.DB_NAME,
     host: env.DB_HOST,
     port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
+    user: encodeURIComponent(env.DB_USER),
+    password: encodeURIComponent(env.DB_PASSWORD),
     collection: 'migrations',
     directory: './migrations'
 };
