1b04b4f8da559d7479bedbb22a80481dcec67050
Roman Stremedlovskyi <Roman_Stremedlovskyi@epam.com>
2016-05-05 09:46:48 +0300

    TG-67

--- src/front/index.js
+++ src/front/index.js
@@ -1,4 +1,13 @@
 import './index.scss';
 import main from './app/main';
 
-main();
+if (process.env.PLATFORM === 'cordova') {
+    const script = document.createElement('script');
+    script.setAttribute('src', 'cordova.js');
+    document.head.appendChild(script);
+    document.addEventListener('deviceready', () => {
+        main();
+    }, false);
+} else {
+    main();
+}
