c76874001070c013277e9d74d291366b3027b012
Borys Ihnatyev <paur.link@gmail.com>
2016-05-11 15:44:27 +0300

    #72 feature(api) wrapper on fetch to make api

--- src/front/app/features/categories/categories.actions.spec.js
+++ src/front/app/features/categories/categories.actions.spec.js
@@ -1,34 +1,41 @@
-import { getCategories, GET_CATEGORIES } from './categories.actions';
+const proxyquire = require('proxyquire').noCallThru();
 
 describe('action/categories', () => {
+    let sut;
     let dispatch;
+    let api;
+    let promise;
+    const categoryList = ['mockedCategories'];
+
+    beforeEach(() => {
+
+        const fetchResponse = {
+            json: env.stub().returns(categoryList)
+        };
+
+        promise = env.stub().resolves(fetchResponse)();
+
+        api = env.stub().returns(promise);
+
+        sut = proxyquire('./categories.actions', {
+            '../../common/api': api
+        });
+
+    });
 
     describe('#getCategories', () => {
         let fetcher;
-        let promise;
-        let fetchResponse;
-        let categoryList;
 
         beforeEach(() => {
-            categoryList = ['mockedCategories'];
 
             dispatch = env.spy();
 
-            fetchResponse = {
-                json: env.stub().returns(categoryList)
-            };
-
-            promise = env.stub().resolves(fetchResponse)();
-
-            global.fetch = env.stub().returns(promise);
-
-            fetcher = getCategories();
+            fetcher = sut.getCategories();
         });
 
         it('should fetch categories', () => {
             fetcher(dispatch);
-
-            global.fetch.should.have.been.calledWith('/api/categories').and.callCount(1);
+            api.should.have.been.calledWith('/api/categories').and.callCount(1);
         });
 
         it('should dispatch categories event with data from response', () => {
@@ -36,7 +43,7 @@ describe('action/categories', () => {
             return promise.finally(() => {
                 const action = dispatch.lastCall.args[0];
                 action.should.eqls({
-                    type: GET_CATEGORIES,
+                    type: sut.GET_CATEGORIES,
                     categories: categoryList
                 });
             });
