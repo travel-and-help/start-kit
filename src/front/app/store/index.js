379ca6d950aee130d38717cc5329db300f27cd8e
Arem_Prokhatskyi <Arem_Prokhatskyi@epam.com>
2016-05-13 13:32:00 +0300

    added form-reducer

--- src/front/app/store/index.js
+++ src/front/app/store/index.js
@@ -4,13 +4,15 @@ import storeEnhancers from './enhancers';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import challenges from '../features/challenges/challenges.reducer';
 import categories from '../features/categories/categories.reducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default () => {
     const state = createStore(
         combineReducers({
             challenges,
             categories,
-            routing: routerReducer
+            routing: routerReducer,
+            form: formReducer
         }),
         storeEnhancers
     );
