d5185a8aad3d7862e1c02a4819acb7cd54753f2f
Saif Adnan <saif_adnan@epam.com>
2016-05-17 09:30:49 +0300

    add components to profile feature

--- src/front/app/features/profile/components/Profile.js
+++ src/front/app/features/profile/components/Profile.js
@@ -1,5 +1,11 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import ChallengeList from '../../../common/components/challenge/ChallengeTileList';
+import UserDetailsContainer from './UserDetails/UserDetailsContainer';
+import SocialList from './Social/SocialList';
+import Header from './Header';
+
 
 class Profile extends Component {
     componentDidMount() {
@@ -9,12 +15,65 @@ class Profile extends Component {
     render() {
         const { user } = this.props;
 
-        console.log(user);
+        return (
+            <div className="profile">
+                <Header />
+                <UserDetailsContainer user={user} />
+
+                <section className="section-container" >
+                    <h4 className="section-container__title" >LOCATIONS</h4>
+                    <div className="section-container__description" >{user.locations}</div>
+                </section>
+
+                <section className="section-container" >
+                    <h4 className="section-container__title" >CATEGORIES</h4>
+                    <div className="section-container__description" >{user.categories}</div>
+                </section>
+
+                <section className="section-container section-container--inline" >
+                    <h4 className="section-container__title" >ON WEB</h4>
+                    <div className="section-container__description" >
+                        <SocialList socials={user.socials} />
+                    </div>
+                </section>
+
+                <section className="section-container" >
+                    <div className="section-container__description" >
+                        <p>
+                            Member since {user.registerDate}
+                        </p>
+                        <p>
+                            Last login {user.lastLogin}
+                        </p>
+                    </div>
+                </section>
+
+                <section className="section-container" >
+                    <h4 className="section-container__title" >Created Challenges</h4>
+                    <div className="section-container__description" >
+                        <ChallengeList challenges={user.createdChallenges} />
+                    </div>
+                    <Link to="Challenges">
+                        Show All
+                    </Link>
+                </section>
+
+                <section className="section-container" >
+                    <h4 className="section-container__title" >Completed Challenges</h4>
+                    <div className="section-container__description" >
+                        <ChallengeList challenges={user.completedChallenges} />
+                    </div>
+                    <Link to="Challenges">
+                        Show All
+                    </Link>
+                </section>
+            </div>
+        );
     }
 }
 
 Profile.propTypes = {
-    user: ImmutablePropTypes.object.isRequired,
+    user: ImmutablePropTypes.map.isRequired,
     getUser: PropTypes.func.isRequired
 };
 
