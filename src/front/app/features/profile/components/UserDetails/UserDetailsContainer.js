ca30adb2c1df71ee81383fc8be3575d90b6bf6c2
Maksym Lysenko <Maksym_Lysenko@epam.com>
2016-05-18 18:44:46 +0300

    first look fixes

--- src/front/app/features/profile/components/UserDetails/UserDetailsContainer.js
+++ src/front/app/features/profile/components/UserDetails/UserDetailsContainer.js
@@ -1,34 +1,38 @@
 import React, { PropTypes } from 'react';
-import Stats from './Stats';
+//  import Stats from './Stats';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
-const UserDetailsContainer = ({ user }) => (
-  <section className="user" >
-      <ul className="user__info" >
-          <li className="user__info__item" >
-              <span className="sprite sprite-todo" ></span>
-          </li>
-          <li className="user__info__item" >
-              <img className="user__image" src={user.photo} title={user.name} />
-          </li>
-          <li className="user__info__item user__info__item--rate" >
-              <span className="sprite sprite-rate" >{user.rating}</span>
-          </li>
-      </ul>
+const UserDetailsContainer = ({ user }) => {
+    const {
+        photo,
+        firstName,
+        rating } = user.toJS();
+    return (
+        <section className="user" >
+            <ul className="user__info" >
+                <li className="user__info__item" >
+                    <img className="user__image" src={photo} title={firstName} />
+                </li>
+                <li className="user__info__item user__info__item--rate" >
+                    <span className="sprite sprite-rate" >{rating}</span>
+                </li>
+            </ul>
 
-      <h2 className="user__name" >{user.name}</h2>
+            <h2 className="user__name" >{firstName}</h2>
+        </section>
+    );
+};
 
-      <Stats stats={user.stats} />
-    </section>
-);
+//  <Stats stats={user.stats} />
+
 
 UserDetailsContainer.propTypes = {
     user: ImmutablePropTypes.mapContains({
-        name: PropTypes.string.isRequired,
+        firstName: PropTypes.string.isRequired,
         photo: PropTypes.string,
-        rating: PropTypes.string,
-        stats: PropTypes.list,
-    }).isRequired,
+        rating: PropTypes.number
+        //  stats: PropTypes.list
+    }).isRequired
 };
 
 export default UserDetailsContainer;
