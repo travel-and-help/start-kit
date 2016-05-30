cc7b0730eb79df18072a9efc2138f48ec8864759
oleksii_novakov <Oleksii_Novakov@epam.com>
2016-05-30 13:11:40 +0300

    Feature #32, new design partly implemented

--- src/front/app/features/challenge/components/ChallengeDetails.js
+++ src/front/app/features/challenge/components/ChallengeDetails.js
@@ -9,75 +9,79 @@ const ChallengeDetails = ({ challenge }) => {
         title,
         level,
         description,
+        categories,
+        location,
         user: {
             firstName,
-            lastName,
-            rating
+            lastName
         }
     } = challenge.toJS();
 
     return (
         <section>
-            <header className="challenge-header" style={{ backgroundImage: `url(${image})` }}>
-                <h1 className="challenge-header__title">{title}</h1>
+            <header className="challenge-header" style={{ backgroundImage: `url(${image})` }} >
+                <h1 className="challenge-header__title" >{title}</h1>
             </header>
 
-            <div className="challenge-info">
+            <div className="challenge-info" >
 
-            <div className="challenge-info__inner challenge-info__inner_big">
+                <div className="challenge-info__inner challenge-info__inner_big" >
 
-                <div className="challenge-info__item">
-                    <Fasteners className="challenge-tile-wrap__fasteners" />
-                    <i className="challenge-info-category__icon icon icon_size-36">group</i>
-                    <span className="challenge-info-category__name">{categories}</span>
-                </div>
+                    <div className="challenge-info__item" >
+                        <Fasteners className="challenge-tile-wrap__fasteners" />
+                        <i className="challenge-info-category__icon icon icon_size-36" >group</i>
+                        <span className="challenge-info-category__name" >{categories}</span>
+                    </div>
 
 
-                <div className="challenge-info__item challenge-info__item_big">
-                    <div className="challenge-info-author">
-                        <span className="challenge-info-author__label">Created by</span>
-                        <span className="challenge-info-author__name">
+                    <div className="challenge-info__item challenge-info__item_big" >
+                        <div className="challenge-info-author" >
+                            <span className="challenge-info-author__label" >Created by</span>
+                        <span className="challenge-info-author__name" >
                             {firstName} {lastName}
                         </span>
+                        </div>
+                        <p className="challenge-info-location" >
+                            <span className="challenge-info-location__label" >Region</span>
+                            <span className="challenge-info-location__name" >{location}</span>
+                        </p>
                     </div>
-                    <p className="challenge-info-location">
-                        <span className="challenge-info-location__label">Region</span>
-                        <span className="challenge-info-location__name">{location}</span>
-                    </p>
-                </div>
 
-                <div className="challenge-info__item challenge-info-level">
-                    <span className="challenge-info-level__value">{level}</span>
-                </div>
+                    <div className="challenge-info__item challenge-info-level" >
+                        <span className="challenge-info-level__value" >{level}</span>
+                    </div>
 
-                <div className="challenge-info__item
+                    <div className="challenge-info__item
                     challenge-info__item_big
                     challenge-info-completed"
-                >
-                    <p className="challenge-info-completed__count"><b>25</b> completions</p>
+                    >
+                        <p className="challenge-info-completed__count" ><b>25</b> completions</p>
+                    </div>
                 </div>
+
+                <div className="challenge-info__inner challenge-info__inner_small" >
+                    <IconButton
+                        title={'Accept'}
+                        buttonClassName={'challenge-info__button'}
+                        iconName={'check_circle'}
+                        iconSize={32}
+                    />
+
+                    <IconButton
+                        title={'Share'}
+                        buttonClassName={'challenge-info__button'}
+                        iconName={'share'}
+                        iconSize={32}
+                    />
+                </div>
+
+                <p className="challenge-info__item challenge-info__item_description">
+                    {description}
+                </p>
             </div>
-
-            <div className="challenge-info__inner challenge-info__inner_small">
-                <IconButton
-                  title={'Accept'}
-                  buttonClassName={'challenge-info__button'}
-                  iconName={'check_circle'}
-                  iconSize={32}
-                />
-
-                <IconButton
-                  title={'Share'}
-                  buttonClassName={'challenge-info__button'}
-                  iconName={'share'}
-                  iconSize={32}
-                />
-            </div>
-
-            <p className="challenge-info__item challenge-info__item_description">{description}</p>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 ChallengeDetails.propTypes = {
     challenge: ImmutablePropTypes.mapContains({
@@ -85,10 +89,11 @@ ChallengeDetails.propTypes = {
         title: PropTypes.string.isRequired,
         level: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        categories: PropTypes.array.isRequired,
+        location: PropTypes.string.isRequired,
         user: ImmutablePropTypes.mapContains({
             firstName: PropTypes.string.isRequired,
-            lastName: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
+            lastName: PropTypes.string.isRequired
         }).isRequired
     }).isRequired
 };
