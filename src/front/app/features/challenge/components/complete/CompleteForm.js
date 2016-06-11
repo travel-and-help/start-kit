845e22fbc6fb3942ddd9a0e160157f7a94ca1347
Maksym Tarasenko <Maksym_Tarasenko@epam.com>
2016-06-12 00:11:04 +0300

    change submit button location

--- src/front/app/features/challenge/components/complete/CompleteForm.js
+++ src/front/app/features/challenge/components/complete/CompleteForm.js
@@ -5,6 +5,8 @@ const CreateFormBody = () => (
     <div className="challenge-complete__body">
         <div className="challenge-complete__body-title">Good job!</div>
         <div className="create-photo"></div>
+
+        <button type="button">Submit</button>
     </div>
 );
 
