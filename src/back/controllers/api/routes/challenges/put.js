f0379d1e23fcf17729f8029c4207215cbaa2a9b0
Andrey Kolesnik <andreas.kolesnik@gmail.com>
2016-06-16 19:59:38 +0300

    fix ESLint errors

--- src/back/controllers/api/routes/challenges/put.js
+++ src/back/controllers/api/routes/challenges/put.js
@@ -7,14 +7,14 @@ const put = (req, res, next) => {
     Challenge.update(
         { _id: req.params.id },
         req.body,
-        (err, challenge)=> {
+        (err, challenge) => {
             if (err) {
                 next(err);
             } else {
                 res.json(challenge);
             }
         }
-    )
+    );
 };
 
 module.exports = put;
