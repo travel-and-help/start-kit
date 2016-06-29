166b1789ee86e61398c379c7907ef3a2833a1046
Iryna Voitova <voitova.iryna@gmail.com>
2016-06-29 17:54:36 +0300

    142 task

--- src/front/app/features/main/profile/ProfileScreenMenu.js
+++ src/front/app/features/main/profile/ProfileScreenMenu.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Menu from '../../Menu';
-import IconButton from '../../../common/components/buttons/IconButton';
 
-const configAction = (
-    <IconButton
-        iconName="settings"
-        iconSize={24}
-    />
-);
 
 const ProfileScreenMenu = () => (
     <Menu
         className="menu_light"
         title="your profile"
-        rightAction={configAction}
     />
 );
 
 export default ProfileScreenMenu;
 
+/* TODO
+Settings_icon
+ import IconButton from '../../../common/components/buttons/IconButton';
+
+ const configAction = (
+ <IconButton
+ iconName="settings"
+ iconSize={24}
+ />
+ );
+
+ Menu
+ rightAction={configAction}
+
+ */
